b09f5fea17a72474873a0f4b3ad741fd6dec8d43
jhumci <julian.huber@mci.edu>
2024-11-07 12:06:30 +0100

    updated with dict

--- reverse_complement.py
+++ reverse_complement.py
@@ -6,27 +6,28 @@ This script should provide the function "reverse_complement" that takes a DNA se
 def reverse_complement(dna_sequence):
     """A Function that returns the reverse complement of a DNA sequence"""
 
+    translation_dict = {"A" : "T",
+                        "T" : "A",
+                        "G" : "C",
+                        "C" : "G"}
+    
     complement = ""
     for base in dna_sequence:
        
-        if base == "A":
-            new_base = "T"
-        elif base == "T":
-            new_base = "A"
-        elif base == "G":
-            new_base = "C"
-        elif base == "C":
-            new_base = "G" 
+        # Wenn es vorkommt
+        if base in translation_dict.keys():
+            new_base = translation_dict[base]
+        
+        # Sonst
         else:
             new_base = "_"
-        
         complement += new_base
 
 
     return complement[::-1]
 
 if __name__ == "__main__":
-    dna_sequence = "ATGATCTCGTAAaaaaa"
+    dna_sequence = "ATGATCTCGTAAa"
     reverse_complement_sequence = reverse_complement(dna_sequence)
     print(dna_sequence)
     print(reverse_complement_sequence)
